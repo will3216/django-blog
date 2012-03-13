cfc243678585b98e3b936334aece581b9148c464
William Bryant <brawrg1@hotmail.com>
2012-03-13 08:37:46 -0500

    attempting to fix under construction page

--- helloo/blog/urls.py
+++ helloo/blog/urls.py
@@ -23,7 +23,24 @@ urlpatterns = patterns('blog.views',
         #Change the [:2] in order to show more than two posts
         queryset=Post.objects.all().order_by("-created")[:2],
         template_name="blog.html")),
-    
+    url(r'^overview$', ListView.as_view(
+        queryset=Post.objects.all().order_by("-created"),
+        template_name="overview.html")),
+    url(r'^technical_drawings$', ListView.as_view(
+        queryset=Post.objects.all().order_by("-created"),
+        template_name="technical_drawings.html")),
+    url(r'^safety$', ListView.as_view(
+        queryset=Post.objects.all().order_by("-created"),
+        template_name="safety.html")),
+    url(r'^measuring$', ListView.as_view(
+        queryset=Post.objects.all().order_by("-created"),
+        template_name="measuring.html")),
+    url(r'^materials$', ListView.as_view(
+        queryset=Post.objects.all().order_by("-created"),
+        template_name="materials.html")),
+    url(r'^machining_and_tools$', ListView.as_view(
+        queryset=Post.objects.all().order_by("-created"),
+        template_name="machining_and_tools.html")),
     url(r'^(?P<pk>\d+)', DetailView.as_view(
         model=Post,
         template_name="post.html")),
@@ -34,4 +51,5 @@ urlpatterns = patterns('blog.views',
     url(r'^feed/$', BlogFeed()),
     url(r'^about/$', direct_to_template, {'template': 'about.html'}),
     url(r'^contact/$', direct_to_template, {'template': 'contact.html'}),
+    url(r'^construction/$', direct_to_template, {'template': 'construction.html'}),
 )
