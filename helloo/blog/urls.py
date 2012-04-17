ddff87deb71e8465a1d211b8b8ff09714c9cf4f0
William Bryant <brawrg1@hotmail.com>
2012-04-16 21:46:41 -0500

    link to pdf of cv perhaps

--- helloo/blog/urls.py
+++ helloo/blog/urls.py
@@ -25,22 +25,22 @@ urlpatterns = patterns('blog.views',
         queryset=Post.objects.filter(tags__name__in=["news"]).order_by("-created")[:5],
         template_name="blog.html")),
     url(r'^overview/$', ListView.as_view(
-        queryset=Post.objects.filter(tags__name__in=["overview"]).order_by("-created"),
+        queryset=Post.objects.filter(tags__name__in=["overview"]).order_by("order"),
         template_name="overview.html")),
     url(r'^technical_drawings/$', ListView.as_view(
-        queryset=Post.objects.filter(tags__name__in=["technical_drawings"]).order_by("-created"),
+        queryset=Post.objects.filter(tags__name__in=["technical_drawings"]).order_by("order"),
         template_name="technical_drawings.html")),
     url(r'^safety/$', ListView.as_view(
-        queryset=Post.objects.filter(tags__name__in=["safety"]).order_by("-created"),
+        queryset=Post.objects.filter(tags__name__in=["safety"]).order_by("order"),
         template_name="safety.html")),
     url(r'^measuring/$', ListView.as_view(
-        queryset=Post.objects.filter(tags__name__in=["measuring"]).order_by("-created"),
+        queryset=Post.objects.filter(tags__name__in=["measuring"]).order_by("order"),
         template_name="measuring.html")),
     url(r'^materials/$', ListView.as_view(
-        queryset=Post.objects.filter(tags__name__in=["materials"]).order_by("-created"),
+        queryset=Post.objects.filter(tags__name__in=["materials"]).order_by("order"),
         template_name="materials.html")),
     url(r'^machining_and_tools/$', ListView.as_view(
-        queryset=Post.objects.filter(tags__name__in=["machining_and_tools"]).order_by("-created"),
+        queryset=Post.objects.filter(tags__name__in=["machining_and_tools"]).order_by("order"),
         template_name="machining_and_tools.html")),
     url(r'^(?P<pk>\d+)', DetailView.as_view(
         model=Post,
@@ -68,8 +68,14 @@ urlpatterns = patterns('blog.views',
         template_name="2011.html")),
     url(r'^tag/(?P<tag>\w+)$', 'tagpage'),
     url(r'^feed/$', BlogFeed()),
-    url(r'^about/$', direct_to_template, {'template': 'about.html'}),
-    url(r'^contact/$', direct_to_template, {'template': 'contact.html'}),
+    url(r'^about/$', ListView.as_view(
+        queryset=Post.objects.filter(tags__name__in=["about"]).order_by("order"),
+        template_name="about.html")),
+    url(r'^contact/$', ListView.as_view(
+        queryset=Post.objects.filter(tags__name__in=["contact"]).order_by("order"),
+        template_name="contact.html")),
     url(r'^curriculum_vitae/$', direct_to_template, {'template': 'cv.html'}),
+    url(r'^curriculum_vitae/(?P<path>.*)$', 'django.views.static.serve',
+        {'/static/': '/static/misc'}),
     url(r'^construction/$', direct_to_template, {'template': 'construction.html'}),
 )
